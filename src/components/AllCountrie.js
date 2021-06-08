7c5b2144efc1f2971b94d89df06d9407ee96dc23
Sanon Steeve <steevesanon6@gmail.com>
2021-06-08 09:58:10 -0400

    paginate with react

--- src/components/AllCountrie.js
+++ src/components/AllCountrie.js
@@ -2,29 +2,55 @@ import react, { useState, useEffect, Component } from 'react'
 import axios from 'axios'
 import CardCountry from './CardCountry'
 import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
-class AllCountrie extends Component {
-    state = {
-        country: []
-    }
+import ReactPaginate from 'react-paginate';
+import '../assets/css/styles.css'
+const AllCountrie=()=>{
+    const [offset, setOffset] = useState(0);
+    const [data, setData] = useState([]);
+    const [perPage] = useState(8);
+    const [pageCount, setPageCount] = useState(0)
 
-    componentDidMount() {
-        axios.get('http://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag')
-            .then(res => {
-                console.log(res)
-                this.setState({ country: res.data });
-            })
+    const getData = async() => {
+        const res = await axios.get(`http://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag`)
+        const data = res.data;
+                  const slice = data.slice(offset, offset + perPage)
+                  const postData = (<MDBRow >{
+                    slice.map(pd =>
+                        <MDBCol md='4' className='col-example'>
+                          <CardCountry detail={pd} />
+                      </MDBCol>
+                    )
+                      }</MDBRow>)
+                  setData(postData)
+                  setPageCount(Math.ceil(data.length / perPage))
     }
-    render() {
-        return ( <MDBRow>
-            {
-                this.state.country.map(person => (
-                    <MDBCol md='4' className='col-example'>
-                        <CardCountry detail={person} />
-                    </MDBCol>
-                ))
-            }
-         </MDBRow>)
-    }
+    const handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        setOffset(selectedPage + 1)
+    };
+    
+     useEffect(() => {
+       getData()
+     }, [offset])
+
+     return (
+        <div className="App">
+          {data}
+           <ReactPaginate
+                        previousLabel={"prev"}
+                        nextLabel={"next"}
+                        breakLabel={"..."}
+                        breakClassName={"break-me"}
+                        pageCount={pageCount}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={5}
+                        onPageChange={handlePageClick}
+                        containerClassName={"pagination"}
+                        subContainerClassName={"pages pagination"}
+                        activeClassName={"active"}/>
+        </div>
+      );
+    
 
 }
 export default AllCountrie
